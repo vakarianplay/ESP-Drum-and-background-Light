b6e9b3296ae5ea7912d101fe9d2c59d2fb60c7da
Kostya Vakarian <vakarianplay@gmail.com>
2022-04-29 14:01:09 +0300

    Update moving sensor function

--- ESP-Cats/src/main.cpp
+++ ESP-Cats/src/main.cpp
@@ -13,33 +13,64 @@ void connectWiFi() {
 
 void mode1() {
   /* вжжжжж 1 */
+  Serial << "mode1" << endl;
 }
 
 void mode2() {
   /* вжжжжж 2 */
+  Serial << "mode2" << endl;
 }
 
 void mode3() {
   /* вжжжжж 3 */
+  Serial << "mode3" << endl;
+}
+
+void chooseMode(){
+  // int mode = 0;
+    curMode = 1 + rand() % 3;
+
+    switch (curMode) {
+    case 1: {
+        mode1();
+        break;
+    }
+    case 2: {
+        mode2();
+        break;
+    }
+    case 3: {
+        mode3();
+        break;
+    }
+    }
+}
+
+void sensorDetect() {
+  if (movingFlag) {
+    if (digitalRead(sensorPin) == 1) {
+      if (millis() - lastTime > 5000) { //delay (5000)
+        chooseMode();
+        lastTime = millis();
+      }
+    }
+  }
 }
 
 void newMsg(FB_msg& msg) {
   if (msg.text == "РЕЖИМ 1") {
     bot.editMenuID(bot.lastBotMsg(), menu1, "");
     bot.sendMessage("Вращение 1", msg.chatID);
-    bot.showMenuText(menu1, msg.chatID);
     mode1();
   }
   if (msg.text == "РЕЖИМ 2") {
     bot.editMenuID(bot.lastBotMsg(), menu1, "");
     bot.sendMessage("Вращение 2", msg.chatID);
-    bot.showMenuText(menu1, msg.chatID);
     mode2();
   }
   if (msg.text == "РЕЖИМ 3") {
     bot.editMenuID(bot.lastBotMsg(), menu1, "");
     bot.sendMessage("Вращение 3", msg.chatID);
-    bot.showMenuText(menu1, msg.chatID);
     mode3();
   }
   if (msg.text == "Датчик движения") {
@@ -51,8 +82,10 @@ void newMsg(FB_msg& msg) {
       movingFlag = 1;
       bot.sendMessage("Датчик движения включен", msg.chatID);
     }
-    bot.showMenuText(menu1, msg.chatID);
-
+  }
+  if (msg.text == "/start") {
+    bot.editMenuID(bot.lastBotMsg(), menu1, "");
+    bot.showMenu(menu1, msg.chatID);
   }
 
   Serial << msg.text << endl;
@@ -63,7 +96,6 @@ void setup() {
   Serial.begin(9600);
   Serial.println();
   bot.attach(newMsg);
-  bot.showMenu(menu1);
 
   pinMode(15, OUTPUT);
   pinMode(13, OUTPUT);
@@ -72,4 +104,5 @@ void setup() {
 
 void loop() {
   bot.tick();
+  sensorDetect();
 }
