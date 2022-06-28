a8b0e1cfc1aacdaa14c20de12b28239426af9de7
Kostya Vakarian <vakarianplay@gmail.com>
2022-06-29 00:25:26 +0300

    Add wifi manager

--- ESP-Cats/src/main.h
+++ ESP-Cats/src/main.h
@@ -1,18 +1,21 @@
 #include <ESP8266WiFi.h>
-#include <Servo.h>
 #include <FastBot.h>
 #include <Streaming.h>
-#define WIFI_SSID "SSID HERE"
-#define WIFI_PASS "WIFI PASS HERE"
-#define BOT_TOKEN "API KEY"
-#define CHAT_ID "CHAT ID" //optional
+#include <ESP8266HTTPUpdateServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+#define WIFI_SSID "NullTempNet"
+#define WIFI_PASS "12345678"
+#define BOT_TOKEN "API"
+#define CHAT_ID "1401259988"
 
 FastBot bot(BOT_TOKEN);
+WiFiManager wifiManager;
 ESP8266HTTPUpdateServer httpUpdater;
 ESP8266WebServer httpServer(80);
 
-String menu1 = ("РЕЖИМ 1 \t РЕЖИМ 2 \t РЕЖИМ 3 \n Датчик движения \n Обновление прошивки");
-String verBuild = "Версия прошивки: 0.03 night";
+String menu1 = ("РЕЖИМ 1 \t РЕЖИМ 2 \t РЕЖИМ 3 \n Датчик движения \n Обновление прошивки \t Состояние сети");
+String verBuild = "Версия прошивки: 0.04 test";
 bool movingFlag = 1;
 const int sensorPin = 15;
 int curMode;
